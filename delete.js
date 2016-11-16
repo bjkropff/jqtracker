e4af87370c6ec902cee81e9692fd414db942ec77
Brian Kropff <brian.kropff@MacBookAir-BrianKropff.domain>
2016-11-15 23:46:39 -0800

    Add next function

--- delete.js
+++
@@ -1,9 +0,0 @@
-// SETUP LIST OF CHARACTERS
-$(document).ready(function() {
-  //event.preventDefault();
-  //====================================//
-  //DELETE FUNCTION button - DONE
-  $( ".btn-danger" ).on( "click", function( event ) {
-    $( this  ).parent('li').hide();
-  });
-})
---
+++ next.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  event.preventDefault();
+  //==========================//
+  //EDIT FUNCTION button
+
+  $( "button#next" ).on( "click", function( event ) {
+    console.log("this thing");
+
+    var shiftItem = list[0];
+    list.shift();
+    list.push(shiftItem);
+
+    $( '#testlist' ).children('li ').hide();
+
+    for(var i = 0; i < list.length; i++){
+      $('ul#testlist').append($('<li> <button class="btn btn-primary notSelected" id="'+ list[i].id +'"" name="select">'+ list[i].name + '</button></br>Init: '
+        + list[i].init + " | AC: "
+        + list[i].ac + " | HP: " + list[i].hp
+        +  ' <button class="btn btn-danger " name="delete">X</button></li>'
+      ));
+    }
+
+    $( ".btn-danger" ).on( "click", function( event ) {
+      $( this  ).parent('li').hide();
+    });
+
+  });
+});
